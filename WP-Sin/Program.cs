790b4fd5f64a33fbbf7e02a514b4e73cac2251dd
Niko V.C. <niko.vondracekchavez@elev.ga.ntig.se>
2025-11-18 10:43:29 +0100

    Initial Commit

---
+++ WP-Sin/Program.cs
@@ -0,0 +1,15 @@
+﻿// Introductions to game, tell player choose a sin
+
+// Player chooses a sin and have it recorded
+
+// Have player distríbute stats points to improve their capabilities
+
+// Ask the player if they want to play with the ability to see the difficulty of skill checks or not
+
+//Begin story and do stuff
+
+// Have dialouge and skill checks, with extra sin actions and addons to the dialogue based on your sin
+
+// Multiple endings based on Public Opinion and whether you win or lose at the end, your sin also gives an extra little add on in every ending
+
+// 5-7 endings, public opinion low, public opinion high, public opinion medium, win, lose, death and so on
